9053b7d0f0bf70209c60e5a4cf8cfa7713eba677
Vincent T <vincent.turrin@gmail.com>
2021-02-23 21:41:23 +0100

    parse int

--- dopamine/quota.py
+++ dopamine/quota.py
@@ -6,7 +6,7 @@ import threading
 class Quota:
     def __init__(self, discord_user):
         self.discord_user = discord_user
-        self.start_quota = os.getenv('START_USER_QUOTA', 3)
+        self.start_quota = int(os.getenv('START_USER_QUOTA', 3))
         self.quota_remaining = self.start_quota
         self.refresh_quota()
 
@@ -16,7 +16,7 @@ class Quota:
             self.quota_remaining += 1
 
         # Add one credit every 1800s == 30min
-        threading.Timer(os.getenv('REFRESH_QUOTA_TIMER', 1800), self.refresh_quota).start()
+        threading.Timer(int(os.getenv('REFRESH_QUOTA_TIMER', 1800)), self.refresh_quota).start()
 
     def use_quota(self):
         if self.quota_remaining > 0:
